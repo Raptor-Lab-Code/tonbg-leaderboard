8c7e266e27094bd8e43f0d6ac60215d8ebd3597c
QuentinNev <neves.quentin@gmail.com>
2025-02-13 18:03:04 +0100

    whatever

--- src/components/LBRow.tsx
+++ src/components/LBRow.tsx
@@ -14,7 +14,7 @@ export type LBRowProps = {
 const LBRow: FC<LBRowProps> = ({ rank, name, logo, score }) => (
     <div className={`flex flex-row text-center items-center bg-black bg-opacity-80 w-full justify-between p-2 border-yellow-100 border-[1px]`}>
         <div className='w-16'>{toOrdinal(rank)}</div>
-        <image height={35} width={35} className='rounded-full ml-4' />
+        <img src={logo} height={35} width={35} className='rounded-full ml-4' />
         <div className='flex flex-row justify-between w-full ml-4'>
             <div>{name}</div>
             <div>{score}</div>
--- src/pages/Airdrop.tsx
+++ src/pages/Airdrop.tsx
@@ -1,20 +1,46 @@
 'use client';
 
 import { useLocation } from "react-router-dom";
+import LBRow from "@/components/LBRow";
 
 export default function Airdrop() {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
 
-  // Get query parameters
-  const param1 = queryParams.get("param1");
-  const param2 = queryParams.get("param2");
+    // Get query parameters
+    const param1 = queryParams.get("param1");
+    const param2 = queryParams.get("param2");
 
-  return (
-    <div>
-      <h1>Airdrop Page</h1>
-      <p>Param 1: {param1}</p>
-      <p>Param 2: {param2}</p>
-    </div>
-  );
+    const partners = [
+        {
+            partnerName: 'swagman',
+            icon: '/background.jpg',
+            score: 1
+        },
+        {
+            partnerName: 'swagman',
+            icon: '/background.jpg',
+            score: 1
+        },
+        {
+            partnerName: 'swagman',
+            icon: '/background.jpg',
+            score: 1
+        },
+        {
+            partnerName: 'swagman',
+            icon: '/background.jpg',
+            score: 1
+        },
+    ]
+
+    return (
+        <div>
+            <div className="flex flex-col w-full flex-1 items-center justify-start gap-2 overflow-y-auto scrollbar-hide mt-[72px] mb-[56px]">
+                {partners.map((quest, index) =>
+                    <LBRow key={index} rank={index + 1} name={quest.partnerName} logo={quest.icon} score={quest.score} />
+                )}
+            </div>
+        </div>
+    );
 }
