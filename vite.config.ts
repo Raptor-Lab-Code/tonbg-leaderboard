2ddad0d6936ac2944ccb651051d5c7c8c2056180
QuentinNev <neves.quentin@gmail.com>
2025-02-18 11:36:42 +0100

    Optimize pictures on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,73 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
+import {ViteImageOptimizer} from 'vite-plugin-image-optimizer';
+
 import path from 'path';
 
-// https://vite.dev/config/
+const DEFAULT_OPTIONS = {
+  test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+  exclude: undefined,
+  include: undefined,
+  includePublic: true,
+  logStats: true,
+  ansiColors: true,
+  svg: {
+    multipass: true,
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            cleanupNumericValues: false,
+            removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+          },
+          cleanupIDs: {
+            minify: false,
+            remove: false,
+          },
+          convertPathData: false,
+        },
+      },
+      'sortAttrs',
+      {
+        name: 'addAttributesToSVGElement',
+        params: {
+          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+        },
+      },
+    ],
+  },
+  png: {
+    // https://sharp.pixelplumbing.com/api-output#png
+    quality: 80,
+  },
+  jpeg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 80,
+  },
+  jpg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 80,
+  },
+  tiff: {
+    // https://sharp.pixelplumbing.com/api-output#tiff
+    quality: 80,
+  },
+  // gif does not support lossless compression
+  // https://sharp.pixelplumbing.com/api-output#gif
+  gif: {},
+  avif: {
+    // https://sharp.pixelplumbing.com/api-output#avif
+    lossless: true,
+  },
+  cache: false,
+  cacheLocation: undefined,
+};
+
 export default defineConfig({
   plugins: [
+    ViteImageOptimizer(DEFAULT_OPTIONS),
     react(),
     tailwindcss()
   ],
@@ -15,4 +77,4 @@ export default defineConfig({
       '@public': path.resolve(__dirname, 'public'), 
     },
   },
-})
+});
