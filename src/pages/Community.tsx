4aacc2919cb3a5156d477a72219fcb65a81a5db6
QuentinNev <neves.quentin@gmail.com>
2025-02-18 01:53:51 +0100

    Test data

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -4,7 +4,18 @@ import { Link, useLocation } from "react-router-dom";
 import LBRow from "@/components/LBRow";
 import { useEffect, useState } from "react";
 
-import testData from '../assets/CommunityTournamentTestData.json';
+const testData = [
+    { PlayerID: "123458999", PlayerName: "Dev", Points: 84, Rank: 1 },
+    { PlayerID: "123458999", PlayerName: "Test 1", Points: 82, Rank: 2 },
+    { PlayerID: "123458999", PlayerName: "Test 2", Points: 72, Rank: 3 },
+    { PlayerID: "123458999", PlayerName: "Test 3", Points: 60, Rank: 4 },
+    { PlayerID: "123458999", PlayerName: "Test 4", Points: 57, Rank: 5 },
+    { PlayerID: "123458999", PlayerName: "Test 5", Points: 56, Rank: 6 },
+    { PlayerID: "123458999", PlayerName: "Test 6", Points: 45, Rank: 7 },
+    { PlayerID: "123458999", PlayerName: "Test 7", Points: 23, Rank: 8 },
+    { PlayerID: "123458999", PlayerName: "Test 8", Points: 14, Rank: 9 },
+    { PlayerID: "123458999", PlayerName: "Test 9", Points: 3, Rank: 10 },
+]
 
 type Player = {
     PlayerID: string;
@@ -31,8 +42,8 @@ export default function Airdrop() {
 
             const communityData = result.find((d: any) => d.Community == community);
 
-            console.log(communityData.Leaderboard)
-            setRanks(communityData?.Leaderboard || []);
+            //setRanks(communityData?.Leaderboard || []);
+            setRanks(testData);
 
             const user = communityData?.Leaderboard.find((p: any) => p.PlayerID == userId?.toString())
 
