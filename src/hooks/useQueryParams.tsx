487d01f8314dcec7dedee75c422356b975dbc600
QuentinNev <neves.quentin@gmail.com>
2025-02-25 10:39:12 +0100

    Add community

--- src/hooks/useQueryParams.tsx
+++ src/hooks/useQueryParams.tsx
@@ -3,13 +3,14 @@ import { useLocation } from "react-router-dom";
 export default function useQueryParams() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
+
     const userId = queryParams.get('userId');
     const embedded = queryParams.get('embedded') === 'true';
-
-    console.log(`userid`, userId);
+    const community = queryParams.get('community');
 
     return {
         userId,
-        embedded
+        embedded,
+        community
     }
 }
