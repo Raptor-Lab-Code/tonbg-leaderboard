d04ad94772d5909ed68d7afa3732d695ce8a3ca9
QuentinNev <neves.quentin@gmail.com>
2025-04-01 15:17:03 +0200

    Finish S2 Leaderbaord

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import CommunityTournament from './pages/CommunityTournament'
 import CommunityDetail from './pages/CommunityDetail';
 import './App.css'
 import SeasonalLeaderboard from "./pages/SeasonalLeaderboard";
@@ -13,11 +12,11 @@ function App() {
     <div className="flex justify-center max-w-[600px] w-screen h-screen p-4">
       <Router>
         <Routes>
-        <Route path="/" element={<Season2Leaderboard />} />
-        <Route path="/Community" element={<CommunityDetail />} />
-        <Route path="/Season1" element={<SeasonalLeaderboard />} />
-        <Route path="/Season2" element={<Season2Leaderboard />} />
-        <Route path="/WhaleIO" element={<WhaleIOLeaderboard />} />
+          <Route path="/" element={<Season2Leaderboard />} />
+          <Route path="/Community" element={<CommunityDetail />} />
+          <Route path="/Season1" element={<SeasonalLeaderboard />} />
+          <Route path="/Season2" element={<Season2Leaderboard />} />
+          <Route path="/WhaleIO" element={<WhaleIOLeaderboard />} />
         </Routes>
       </Router>
     </div>
