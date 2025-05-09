2e6cc67976de109a59ae7926a9bbd78c767d0ee3
QuentinNev <neves.quentin@gmail.com>
2025-05-10 00:40:06 +0200

    Change opacity

--- src/components/LBRow.tsx
+++ src/components/LBRow.tsx
@@ -14,7 +14,7 @@ export type LBRowProps = {
 }
 
 const LBRow: FC<LBRowProps> = ({ rank, name, logo, score, className, style }) => (
-    <div style={style} className={`flex flex-row text-center items-center bg-black/90 w-full justify-between p-1 border-yellow-100 border-[1px] ${className}`}>
+    <div style={style} className={`flex flex-row text-center items-center bg-black/75 w-full justify-between p-1 border-yellow-100 border-[1px] ${className}`}>
         <div className='w-16'>{toOrdinal(rank)}</div>
         {logo && <img src={logo} height={35} width={35} className='rounded-full ml-4' />}
         <div className='flex flex-row justify-between w-full ml-4 mr-4'>
